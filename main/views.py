04e5bb4f49665ecf6a5bc7e62badf5f5a1a1a2c4
Fabio Martinelli <webmaster@zupy.com.br>
2022-07-24 17:16:46 -0300

    Templates Basic

--- main/views.py
+++ main/views.py
@@ -4,4 +4,7 @@ from .models import ToDoList, Item
 # Create your views here.
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
-    return HttpResponse("<h1>%s</h1>" %ls.name)
+    return render(response, "main/list.html", {"ls":ls})
+ 
+def home(response):
+    return render(response, "main/home.html", {})
