9b71315b4647214c4f15c157cb6924972f163506
Fabio Martinelli <webmaster@zupy.com.br>
2022-07-24 18:09:09 -0300

    Defining forms

--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import is_valid_path
+from .forms import CreateNewList
 from .models import ToDoList, Item
 # Create your views here.
 def index(response, id):
@@ -8,3 +10,21 @@ def index(response, id):
  
 def home(response):
     return render(response, "main/home.html", {})
+
+def create(response):
+    #Pega od dados do formulário enviado via post e salva na nova lista
+    if response.method == "POST":
+        form = CreateNewList(response.POST)
+    #Verifica os dados e limpa e salva o nome da lista    
+        if form.is_valid():
+            n = form.cleaned_data["name"]
+            t = ToDoList(name=n)
+            t.save()
+            
+     #Depois de salvar, redireciona para a lista recém criada, passando o ID       
+            
+            return HttpResponseRedirect("/%i" %t.id)
+
+    else: 
+        form = CreateNewList()
+    return render(response, "main/create.html", {"form":form})
